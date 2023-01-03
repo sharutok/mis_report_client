c78325a2313a2da70cd70870bbef1c9d0132c52e
Sharan Kudtarkar <SK19@ador.co.in>
2023-01-04 00:24:45 +0530

    04-01-2023

--- src/Side/Http.js
+++ src/Side/Http.js
@@ -1,16 +1,10 @@
-
-
-
-const backendURL = window.location.port === "3000" ?
-    // "http://localhost" :
-    "http://localhost" :
-    "http://14.143.203.75";
+const backendURL = window.location.port === "3000" ? "http://localhost" : "http://14.143.203.75";
 const backendPORT = window.location.port === "3000" ? "7000" : "3030";
 
 const backendHTTP = `${backendURL}:${backendPORT}`;
 
 export const httpApi = {
-    salesData: `${backendHTTP}/api/sales-data`, 
+    salesData: `${backendHTTP}/api/sales-data`,
     productMixData: `${backendHTTP}/api/product-mix-data`,
     top5SP: `${backendHTTP}/api/top-selling`,
     payables: `${backendHTTP}/api/payables`,
@@ -18,7 +12,5 @@ export const httpApi = {
     otp_sender: `${backendHTTP}/api/get/otp`,
     otp_verify: `${backendHTTP}/api/verify/otp`,
 };
-
-
 
 
